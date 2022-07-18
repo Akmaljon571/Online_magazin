ec24ae28e0bdcf0666c0791b16614fce277d54ec
Akmaljon571 <ahmadjonovakmaljon571@yandex.ru>
2022-07-19 00:00:33 +0500

    finish

--- src/page/error/error.jsx
+++ src/page/error/error.jsx
@@ -1,8 +1,15 @@
 import './error.scss'
+import { Button, Result } from 'antd';
+import React from 'react';
 
 function Error() {
     return ( 
-        'Error'
+        <Result
+            status="404"
+            title="404"
+            subTitle="Sorry, the page you visited does not exist."
+            extra={<Button type="primary">Back Home</Button>}
+        />
      );
 }
 
