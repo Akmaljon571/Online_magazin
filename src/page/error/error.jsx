8e1399d083f2d5b73d89bb46e5d62b8e67bd0582
Akmaljon571 <ahmadjonovakmaljon571@yandex.ru>
2022-07-19 00:18:34 +0500

    finish

--- src/page/error/error.jsx
+++ src/page/error/error.jsx
@@ -1,14 +1,20 @@
 import './error.scss'
 import { Button, Result } from 'antd';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Error() {
+    const navigate = useNavigate()
+   let btn = () => {
+    navigate('/')
+   }
+
     return ( 
         <Result
             status="404"
             title="404"
             subTitle="Sorry, the page you visited does not exist."
-            extra={<Button type="primary">Back Home</Button>}
+            extra={<Button onClick={btn} type="primary">Back Home</Button>}
         />
      );
 }
